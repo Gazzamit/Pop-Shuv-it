bfd2850309a8ce0f582474cc6a7c27471fc9796e
Gazzamit <info@garymitchellonline.co.uk>
2022-11-12 09:39:55 +0000

    Add player to GM Menu Scene

--- Assets/Scripts/Menu/RotatePlayer.cs
+++ Assets/Scripts/Menu/RotatePlayer.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class RotatePlayer : MonoBehaviour
 {
-public float degreesPerSecond = 20;
+public float degreesPerSecond = 30;
     private void Update()
     {
-        transform.Rotate(new Vector3(degreesPerSecond, 0 , 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, degreesPerSecond , 0) * Time.deltaTime);
     }
 }
